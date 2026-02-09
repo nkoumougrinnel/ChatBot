bef3dc3d92a9803d7c1f4f703013dfaf92bbc489
nkoumougrinnel <135821429+nkoumougrinnel@users.noreply.github.com>
2026-02-09 05:54:48 +0100

    Creation des modeles (#3)
    * feat: création des modèles Django pour le chatbot Sup'ptic
    
    - Modèle CustomUser (étend AbstractUser) avec student_id unique
    - Modèle Category avec relations parent/enfant
    - Modèle FAQ avec catégorie, sous-thème et source
    - Modèle FAQVector pour le stockage des embeddings TF-IDF
    - Modèle Feedback pour les retours utilisateurs
    - Contraintes : CASCADE, RESTRICT, SET_NULL selon les besoins
    - Interface admin configurée pour tous les modèles
    - Données de test créées pour démonstration
    
    * correction des apps django
    
    ---------
    
    Co-authored-by: OLAMA Victoire <Aimeolama27@gmail.com>

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    student_id = models.CharField(max_length=20, unique=True, null=True)
+    department = models.CharField(max_length=100, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
+    role=['etudiant', 'admin', 'anonyme']
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Utilisateur"
+        verbose_name_plural = "Utilisateurs"
+
+    def __str__(self):
+        return f"{self.username} ({self.student_id})"
+
+# Create your models here.
